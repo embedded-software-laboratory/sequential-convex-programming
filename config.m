1d3e8a21900498f4b9ff23bcaa7cda323fe89d8c
Theo Henneken <me@DuSTH.de>
2021-05-03 15:01:48 +0200

    CHANGE disable logging for speed

--- config.m
+++ config.m
@@ -14,8 +14,16 @@ cfg.outputPath = ['output\', datestr(now, 'yyyy.mm.dd_HH_MM_SS'), '\'];
 %% Race
 cfg.race.n_laps = 3;            % Number of laps to be driven
 
+%% Logging
+cfg.log.DEBUG   = 4;
+cfg.log.LOG     = 3;
+cfg.log.INFO    = 2;
+cfg.log.WARN    = 1;
+cfg.log.ERROR   = 0;
+cfg.log.level   = cfg.log.DEBUG; % log level: choose from above
+
 %% Plotting
-cfg.plot.is_enabled = true;
+cfg.plot.is_enabled = false;
 cfg.plot.has_accelerations = true;
 cfg.plot.has_focus_on_vehicle = false;
 
