622502724482b78e48c4eece6fe7c47f8eae99c4
Theo Henneken <me@DuSTH.de>
2021-05-04 22:47:03 +0200

    FIX merge poylgon not checking for equality

--- +controller/+SCR/+generate_track_polygons/merge_polygons.m
+++ +controller/+SCR/+generate_track_polygons/merge_polygons.m
@@ -17,6 +17,7 @@ for i = length(track.polygons):-1:2
     if V_merged - V1 - V2 < area_tolerance
         track.polygons(i) = []; % Delete first polygon
         track.polygons(i-1).vertex_indices = idx_merged(K(2:end)); % Replace second polygon with union
+    if volume_merged - (volume_1 + volume_2) <= .0%5 % epsilon_volume_tolerance
     end
 end
 end
