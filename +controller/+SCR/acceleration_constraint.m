5f07c8a23b4c68fa754929e3480d090b0cc94dbe
Theo Henneken <me@DuSTH.de>
2021-05-19 15:00:09 +0200

    FIX SCR acceleration constraint function interface matched to SL and rest of code

--- +controller/+SCR/acceleration_constraint.m
+++ +controller/+SCR/acceleration_constraint.m
@@ -3,7 +3,7 @@
 % i = index of tangent
 
 % Resulting constraint: Ax * [px,py,vx,vy] + Au * [ax,ay] <= b
-function [Au, b] = acceleration_constraint(p,k,~)
+function [Au, b] = acceleration_constraint(~, p,k,~)
      Au = [cos(2*pi.*k/p.n_acceleration_limits) sin(2*pi.*k/p.n_acceleration_limits)];
      b = p.a_max;
 end
