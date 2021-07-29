003d3d931c71356542d29877013ed38887e85cf6
Theo Henneken <me@DuSTH.de>
2021-07-29 18:33:24 +0200

    FIX (again!?) accidentally introduced wrong index

--- +sim/init_log.m
+++ +sim/init_log.m
@@ -8,7 +8,7 @@ fields{2,1} = {};
 log.lap = struct(fields{:});
 
 % copy structure from ws
-fields = fieldnames(ws.vhs{2})';
+fields = fieldnames(ws.vhs{1})';
 fields{2,1} = {};
 for i = 1:length(cfg.scn.vhs)
     log.vehicles{i} = struct(fields{:});   % Struct to save all data for each lap
