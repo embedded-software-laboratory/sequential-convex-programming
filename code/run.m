cce97c94369536d42db567e4a4fcc974da343181
Theo Henneken <me@DuSTH.de>
2021-08-07 12:01:00 +0200

    CHANGE scenarios in run

--- code/run.m
+++ code/run.m
@@ -16,24 +16,29 @@ cfg = config.config();
 scenarios(1) = config.base_scenario(cfg);
 
 % one vehicle only (and endless race)
-scenarios(10) = config.scenario_endless_race(config.scenario_1_vehicle(config.base_scenario(cfg), @config.vehicle_ST_Liniger));
-scenarios(11) = config.scenario_endless_race(config.scenario_1_vehicle(config.base_scenario(cfg), @config.vehicle_lin_Liniger_ST_Liniger));
-scenarios(12) = config.scenario_endless_race(config.scenario_1_vehicle(config.base_scenario(cfg), @config.vehicle_lin_Liniger));
-
-% .. with reduced checkpoints
-scenarios(15) = config.scenario_SL_reduce_checkpoints(config.scenario_endless_race(config.scenario_1_vehicle(config.base_scenario(cfg), @config.vehicle_lin_Liniger)));
+scenarios(10) = config.scenario_1_vehicle(scenarios(1), @config.vehicle_ST_Liniger);
+scenarios(11) = config.scenario_1_vehicle(scenarios(1), @config.vehicle_lin_Liniger_ST_Liniger);
+scenarios(12) = config.scenario_1_vehicle(scenarios(1), @config.vehicle_lin_Liniger);
+% ... and with endless race
+scenarios(13) = config.scenario_endless_race(scenarios(10));
+scenarios(14) = config.scenario_endless_race(scenarios(11));
+scenarios(15) = config.scenario_endless_race(scenarios(12));
+% .. and with reduced checkpoints
+scenarios(16) = config.scenario_SL_reduce_checkpoints(scenarios(13));
+scenarios(17) = config.scenario_SL_reduce_checkpoints(scenarios(14));
+scenarios(18) = config.scenario_SL_reduce_checkpoints(scenarios(15));
 
 % different controller per vehicle
-scenarios(20) = config.scenario_models_comparison_linear(config.base_scenario(cfg));
-scenarios(21) = config.scenario_endless_race(config.scenario_lin_vs_ST(config.base_scenario(cfg)));
+scenarios(20) = config.scenario_models_comparison_linear(scenarios(1));
+scenarios(21) = config.scenario_endless_race(config.scenario_lin_vs_ST(scenarios(1)));
 
 % only one main vehicle, of which state various controllers are calculated
 %   and displayed overlayed
-scenarios(30) = config.scenario_main_vehicle_lin_vs_ST(config.base_scenario(cfg));
-scenarios(31) = config.scenario_main_vehicle_SL_vs_SCR(config.base_scenario(cfg));
+scenarios(30) = config.scenario_main_vehicle_lin_vs_ST(scenarios(1));
+scenarios(31) = config.scenario_main_vehicle_SL_vs_SCR(scenarios(1));
 
 % previous paper comparion
-scenarios(41) = config.scenario_paper_SL(config.base_scenario(cfg));
+scenarios(41) = config.scenario_paper_SL(scenarios(1));
 
 %% Run Selected Scenario
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
