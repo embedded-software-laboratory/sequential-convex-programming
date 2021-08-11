5908661c692d7e59c7a5c6428c84ee6ecac5c978
Theo Henneken <me@DuSTH.de>
2021-08-11 21:39:02 +0200

    FIX removed weights from scenario

--- code/+config/scenario_SL_vs_SCR.m
+++ code/+config/scenario_SL_vs_SCR.m
@@ -6,12 +6,7 @@ cfg.scn.description = [cfg.scn.description '\nwith comparison of SL vs SCR track
 vehicle_default = config.vehicle_ST_Liniger(config.base_vehicle(cfg));
 
 % vehicle 1: SL
-vehicle_default.p.Q = 17; % weight for maximization of position on track
-vehicle_default.p.R = diag([55 9]); % weight for control changes over time
-vehicle_default.p.trust_region_size = 0.11; % SL only
 cfg.scn.vhs{end + 1} = vehicle_default;
 
 % vehicle 2: SCR
-vehicle_default.p.Q = 1; %0.9; % weight for maximization of position on track
-vehicle_default.p.R = diag([90 90]); % weight for control changes over time
 cfg.scn.vhs{end + 1} = config.vehicle_SCR(vehicle_default);
