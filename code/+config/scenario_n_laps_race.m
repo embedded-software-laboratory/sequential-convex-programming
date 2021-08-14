f9c31b0871efea87d8884001905c26fc9403b84e
Theo Henneken <me@DuSTH.de>
2021-08-14 16:06:01 +0200

    FIX scenario description

--- code/+config/scenario_n_laps_race.m
+++ code/+config/scenario_n_laps_race.m
@@ -1,6 +1,6 @@
 function cfg = scenario_n_laps_race(cfg, n_laps)
 % making race endless (or close to)
-cfg.scn.description = [cfg.scn.description '\nwith endless race'];
+cfg.scn.description = [cfg.scn.description '\n' sprintf('with %i laps race', n_laps)];
 
 cfg.race.n_laps = n_laps;            % Number of laps to be driven
 end
