e72b77225e3b3b35cb9c0725badea6c1761f225a
Theo Henneken <me@DuSTH.de>
2021-08-14 11:38:45 +0200

    CHANGE use RWTH colors which are better visible

--- code/+utils/getRwthColors.m
+++ code/+utils/getRwthColors.m
@@ -5,23 +5,21 @@ function colors = getRwthColors(percent)
 colors = zeros(8,3);
 
 if percent == 100 % 100% colors
-    colors(1,:) = [0 84 159]; % blue
-    colors(2,:) = [246 168 0]; % orange
-    colors(3,:) = [87 171 39]; % green
-    colors(4,:) = [204 7 30]; % red
-    colors(5,:) = [0 97 101]; % petrol
-    colors(6,:) = [97 33 88]; % violet
-    colors(7,:) = [87 171 39]; % turquoise
-    colors(1,:) = [142 186 229]; % blue
+    colors(1,:) = [246 168 0]; % orange
+    colors(2,:) = [87 171 39]; % green
+    colors(3,:) = [204 7 30]; % red
+    colors(4,:) = [0 97 101]; % petrol
+    colors(5,:) = [97 33 88]; % violet
+    colors(6,:) = [87 171 39]; % turquoise
+    colors(7,:) = [0 84 159]; % blue
 elseif percent == 50  % 50% colors
-    colors(1,:) = [142 186 229]; % blue
-    colors(2,:) = [253 212 143]; % orange
-    colors(3,:) = [184 214 152]; % green
-    colors(4,:) = [230 150 121]; % red
-    colors(5,:) = [125 164 167]; % petrol
-    colors(6,:) = [168 133 158]; % violet
-    colors(7,:) = [137 204 207]; % turquoise
-    colors(1,:) = [142 186 229]; % blue
+    colors(1,:) = [253 212 143]; % orange
+    colors(2,:) = [184 214 152]; % green
+    colors(3,:) = [230 150 121]; % red
+    colors(4,:) = [125 164 167]; % petrol
+    colors(5,:) = [168 133 158]; % violet
+    colors(6,:) = [137 204 207]; % turquoise
+    colors(7,:) = [142 186 229]; % blue
 end
 
 colors = colors / 255; % Get RGB triplet in values from 0 to 1
