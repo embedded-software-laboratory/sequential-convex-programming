dfc7cda1ae2f55f062b0df027d47d28d4cc8cc87
Theo Henneken <me@DuSTH.de>
2021-08-14 13:08:23 +0200

    ADD export figs as fig

--- code/+utils/exportFigure.m
+++ code/+utils/exportFigure.m
@@ -1,4 +1,6 @@
 function exportFigure(figure_number, filename)
-% save given figure under given path (without file extension) as landscape PDF
+% save given figure under given path (without file extension) as landscape
+% PDF and MATLAB fig
 set(figure_number, 'PaperOrientation', 'landscape');
 print(['-f' num2str(figure_number)], [filename '.pdf'], '-dpdf', '-bestfit');
+saveas(figure_number, [filename '.fig'], 'fig');
